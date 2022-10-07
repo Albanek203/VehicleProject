7985bd4869c9bef1fdcc06ac54307ae6029c750c
Albanek203 <oleglovchuk@ukr.net>
2022-10-07 22:14:43 +0300

    added admin-header

--- frontend/src/app/modules/admin/layout/admin-layout.module.ts
+++ frontend/src/app/modules/admin/layout/admin-layout.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from "@angular/router";
 import { AdminLayoutComponent } from "@modules/admin/layout/admin-layout.component";
+import { AdminHeaderComponent } from './admin-header/admin-header.component';
 
 @NgModule({
   declarations: [
-    AdminLayoutComponent
+    AdminLayoutComponent,
+    AdminHeaderComponent
   ],
   imports: [
     CommonModule,
