97ce9214318bfdd7a83eec5993a124da0d4b0928
Albanek203 <oleglovchuk@ukr.net>
2022-10-07 22:12:41 +0300

    added main-header

--- frontend/src/app/modules/main/layout/main-layout.module.ts
+++ frontend/src/app/modules/main/layout/main-layout.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainLayoutComponent } from "@modules/main/layout/main-layout.component";
 import { RouterOutlet } from "@angular/router";
+import { MainHeaderComponent } from './main-header/main-header.component';
 
 @NgModule({
   declarations: [
-    MainLayoutComponent
+    MainLayoutComponent,
+    MainHeaderComponent
   ],
   imports: [
     CommonModule,
