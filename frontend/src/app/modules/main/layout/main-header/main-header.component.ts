1a588a12cc3ba30f736908c12e9f7f32dbb0c613
Albanek203 <oleglovchuk@ukr.net>
2022-10-10 10:09:59 +0300

    configure authorization visibility

--- frontend/src/app/modules/main/layout/main-header/main-header.component.ts
+++ frontend/src/app/modules/main/layout/main-header/main-header.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MenuItem } from "primeng/api";
 import { first } from "rxjs";
 import { AuthHttpService } from "@api/services/auth-http.service";
+import { SecurityService } from "../../../../services/security.service";
+import { Role } from "@api/models/enum/Role";
 
 @Component({
   selector: 'app-main-header',
@@ -10,55 +12,18 @@ import { AuthHttpService } from "@api/services/auth-http.service";
 })
 export class MainHeaderComponent implements OnInit {
 
-  isLoginIn: boolean = false;
-  userName: string = 'albanekpiss2222@gmail.com';
+  userName: string = this.securityService.getUser()?.email;
 
-  userMenuItems: MenuItem[] = [
-    {
-      label: `Signed in as\n${ this.userName }`
-    },
-    {
-      separator: true,
-      items: [
-        {
-          label: 'Admin panel',
-          icon: 'fa-solid fa-screwdriver-wrench',
-          routerLink: '/admin'
-        }
-      ]
-    },
-    {
-      separator: true,
-      items: [
-        {
-          label: 'Your profile',
-          icon: 'fa-solid fa-user',
-          command: () => {
-            console.log("Link your profile");
-          }
-        },
-        {
-          label: 'Your deliveries',
-          icon: 'fa-solid fa-truck',
-          command: () => {
-            console.log("Link your deliveries");
-          }
-        }
-      ]
-    },
-    {
-      separator: true,
-      items: [
-        {
-          label: 'Sign out',
-          icon: 'fa-solid fa-right-from-bracket',
-          command: () => this.logout()
-        }
-      ]
-    }
-  ];
+  userMenuItems: MenuItem[] = [];
 
-  constructor(private authHttpService: AuthHttpService) {
+  constructor(private authHttpService: AuthHttpService,
+              private securityService: SecurityService) {
+    this.initUserMenuItem();
+
+    // TODO: unsubscribe an destroy
+    this.securityService.isAuthenticated$.subscribe(isAuthenticated => {
+      this.initUserMenuItem();
+    })
   }
 
   ngOnInit(): void {
@@ -68,12 +33,79 @@ export class MainHeaderComponent implements OnInit {
     this.authHttpService.logout()
       .pipe(first())
       .subscribe({
-        next: user => console.log("log out"),
-        error: error => console.log("error log out"),
+        next: user => {
+          console.log("log out");
+          this.securityService.logout();
+        },
+        error: error => console.log(error),
       });
   }
 
-  signUp() {
-    console.log("signUp");
+  initUserMenuItem() {
+    this.userMenuItems = [
+      {
+        label: 'Authorization',
+        items: [
+          {
+            label: 'Sign in',
+            icon: 'fa-solid fa-right-from-bracket',
+            routerLink: '/login'
+          },
+          {
+            label: 'Sign up',
+            icon: 'fa-regular fa-address-card',
+            routerLink: '/registration'
+          }
+        ],
+        visible: !this.securityService.isAuthenticated()
+      },
+      {
+        label: `Signed in as\n${ this.userName }`,
+        visible: this.securityService.isAuthenticated()
+      },
+      {
+        separator: true,
+        items: [
+          {
+            label: 'Admin panel',
+            icon: 'fa-solid fa-screwdriver-wrench',
+            routerLink: '/admin'
+          }
+        ],
+        visible: this.securityService.hasRole(Role.ADMIN)
+      },
+      {
+        separator: true,
+        items: [
+          {
+            label: 'Your profile',
+            icon: 'fa-solid fa-user',
+            command: () => {
+              console.log("Link your profile");
+            }
+          },
+          {
+            label: 'Your deliveries',
+            icon: 'fa-solid fa-truck',
+            command: () => {
+              console.log("Link your deliveries");
+            },
+            visible: this.securityService.hasRole(Role.CUSTOMER)
+          }
+        ],
+        visible: this.securityService.isAuthenticated()
+      },
+      {
+        separator: true,
+        items: [
+          {
+            label: 'Sign out',
+            icon: 'fa-solid fa-right-from-bracket',
+            command: () => this.logout()
+          }
+        ],
+        visible: this.securityService.isAuthenticated()
+      }
+    ];
   }
 }
