89bd4f48b39f83012e5ed8ae56d9f7ada7a86bad
Albanek203 <oleglovchuk@ukr.net>
2022-10-10 18:27:17 +0300

    implement @RequiredArgsConstructor

--- backend/src/main/java/com/transportation/security/SecurityService.java
+++ backend/src/main/java/com/transportation/security/SecurityService.java
@@ -4,14 +4,15 @@ package com.transportation.security;
 import com.transportation.entity.User;
 import com.transportation.exception.AuthenticationRequiredException;
 import com.transportation.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class SecurityService {
-    @Autowired
-    UserRepository userRepository;
+    private final UserRepository userRepository;
 
     public User getUser() {
         return this.userRepository.findByEmail(getCurrentUserEmail()).orElseThrow(AuthenticationRequiredException::new);
