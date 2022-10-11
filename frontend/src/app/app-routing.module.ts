85d2565cbeb58f6a4076a568be4b38bcca8b9f87
Albanek203 <oleglovchuk@ukr.net>
2022-10-11 21:52:34 +0300

    added guard for admin

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminGuard } from "@guards/admin.guard";
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./modules/main/main.module').then(m => m.MainModule) },
-  { path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule) }
+  { path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule), canActivate: [AdminGuard] }
 ];
 
 @NgModule({
