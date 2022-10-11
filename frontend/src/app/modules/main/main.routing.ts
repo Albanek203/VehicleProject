0a27a8b155a3dd225fe818fd11804ecd45ddab22
Albanek203 <oleglovchuk@ukr.net>
2022-10-11 21:46:35 +0300

    configure routes

--- frontend/src/app/modules/main/main.routing.ts
+++ frontend/src/app/modules/main/main.routing.ts
@@ -1,5 +1,7 @@
 import { RouterModule, Routes } from "@angular/router";
 import { MainLayoutComponent } from "@modules/main/layout/main-layout.component";
+import { NotAuthenticatedGuard } from "@guards/not-authenticated.guard";
+import { AuthenticatedGuard } from "@guards/authenticated.guard";
 
 const routes: Routes = [
   {
@@ -7,7 +9,9 @@ const routes: Routes = [
     component: MainLayoutComponent,
     children: [
       { path: '', loadChildren: () => import('./components/dashboard/dashboard.component').then(m => m.DashboardModule) },
-      { path: 'login', loadChildren: () => import('./components/login/login.component').then(m => m.LoginModule) },
+      { path: 'login', loadChildren: () => import('./components/authorization/login/login.component').then(m => m.LoginModule), canActivate: [NotAuthenticatedGuard] },
+      { path: 'registration', loadChildren: () => import('./components/authorization/registration/registration.component').then(m => m.RegistrationModule), canActivate: [NotAuthenticatedGuard] },
+      { path: 'profile', loadChildren: () => import('./components/profile/profile.component').then(m => m.ProfileModule), canActivate:[AuthenticatedGuard] },
       { path: 'deliveries', loadChildren: () => import('./components/deliveries/deliveries.module').then(m => m.DeliveriesModule) }
     ]
   }
