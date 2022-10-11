6b1909476afa20c7a56ebedc064f7c22fb2ae05a
Albanek203 <oleglovchuk@ukr.net>
2022-10-12 00:22:27 +0300

    implement fromPrimeNgDataView function

--- frontend/src/app/api/models/Pagination.ts
+++ frontend/src/app/api/models/Pagination.ts
@@ -16,4 +16,12 @@ export class Pagination {
     if(primeNgEvent.sortField) pagination.sort = `${ primeNgEvent.sortField },${ primeNgEvent.sortOrder > 0 ? 'ASC' : 'DESC' }`
     return pagination;
   }
+
+  static fromPrimeNgDataView(primeNgEvent: any, sort: string): Pagination {
+    let pagination = new Pagination();
+    pagination.page = primeNgEvent.first ? primeNgEvent.first / primeNgEvent.rows : 0;
+    pagination.size = primeNgEvent.rows;
+    pagination.sort = sort;
+    return pagination;
+  }
 }
