1b556501f07fd9710d95e17f9eceb4e7d754f971
Albanek203 <oleglovchuk@ukr.net>
2022-10-12 15:01:59 +0300

    configure route to error interceptors

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { AdminGuard } from "@guards/admin.guard";
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./modules/main/main.module').then(m => m.MainModule) },
-  { path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule), canActivate: [AdminGuard] }
+  { path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule), canActivate: [AdminGuard] },
+  { path: 'error/:id', loadChildren: () => import('./modules/interceptor/interceptor.component').then(m => m.InterceptorModule) }
 ];
 
 @NgModule({
